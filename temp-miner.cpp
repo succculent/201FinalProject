acdf3a1418aa1deae7d92e0b7951491a556919c4
NeckChin <nick.chen053100@gmail.com>
2019-11-26 12:36:00 -0800

    miner test outline

---
+++ temp-miner.cpp
@@ -0,0 +1,20 @@
+#include <cpr/cpr.h> //cpr is a REST API for C++
+
+void miner() {
+	//setup
+	//set difficulty
+	//set max nonce
+	//retrieve block
+	//compute merkle root from transactions once transactions are full
+
+	//mining 
+	// **assume nonce and merkle root already set**
+	//loop nonce increments/randomizes
+	//compute double sha256
+	//check if result is a solution
+}
+
+
+
+//notifying blockchain
+
