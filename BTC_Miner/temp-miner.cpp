8a19c5c2d16548563312df5322cbf3158e1dd69f
NeckChin <nick.chen053100@gmail.com>
2019-11-26 13:20:17 -0800

    make in progress, temp-miner get request in progress

---
+++ BTC_Miner/temp-miner.cpp
@@ -0,0 +1,25 @@
+#include <cpr/cpr.h> //cpr is a REST API for C++
+
+void miner(int argc, char** argv) {
+	// SETUP
+	// retrieve latest block
+	auto latest = cpr::Get(cpr::Url{"https://blockchain.info/latestblock"});
+
+
+
+	//set difficulty
+	//set max nonce
+	//retrieve block
+	//compute merkle root from transactions once transactions are full
+
+	//mining 
+	// **assume nonce and merkle root already set**
+	//loop nonce increments/randomizes
+	//compute double sha256
+	//check if result is a solution
+}
+
+
+
+//notifying blockchain
+
