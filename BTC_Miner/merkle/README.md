fd3b657e669c5f9d1ef00d1a57fd05a14f8fdb72
NeckChin <nick.chen053100@gmail.com>
2019-11-26 20:38:18 -0800

    bitcoin miner client side implementation pretty much done

---
+++ BTC_Miner/merkle/README.md
@@ -0,0 +1,4 @@
+All code in this directory is modified version of code by https://github.com/bitcoin/bitcoin
+
+The code is under MIT license
+
---
+++ miner.py
@@ -0,0 +1,59 @@
+import requests
+import urllib.request
+import json
+import hashlib
+import merkletools
+import random
+import time
+
+NONCE_MAX = 100000
+API_URL = "https://blockchain.info/latestblock"
+TX_KEY = "txIndexes"
+PREVHASH_KEY = "hash"
+HASH_ATTEMPTS = 3000
+THRESHOLD = 3
+
+with urllib.request.urlopen(API_URL) as url:
+    data = json.loads(url.read().decode())
+    prevHash = data[PREVHASH_KEY]
+    transactions = data[TX_KEY]
+
+    #merkle hash transactions
+    mt = merkletools.MerkleTools(hash_type="sha256")
+    for transaction in transactions:
+        mt.add_leaf(hex(transaction), True)
+    mt.make_tree()
+    is_ready = False;
+    while not is_ready:
+        is_ready = mt.is_ready
+    root_value = mt.get_merkle_root();
+
+    #DEBUG
+    print ("Merkle Root: " + root_value)
+    print("Previous Hash: " + prevHash)
+    print("Time: " + str(data["time"]))
+    #print("Transactions: ")
+    #print(transactions)    
+    start_time = time.time()
+    #set up threshold
+    comp = (THRESHOLD) * '0'
+
+    #attempt HASH_ATTEMPTS times to find the correct hash
+    hashdict = {}
+    for i in range(HASH_ATTEMPTS):
+        nonce = random.randrange(NONCE_MAX)
+        firstpass = hashlib.sha256(((root_value+str(nonce)).encode('utf-8')))
+        secondpass = hashlib.sha256()
+        firstpass.hexdigest()
+        secondpass.update(firstpass.digest())
+        secondpass.hexdigest()
+        h = secondpass.hexdigest()
+        #only allows hash through if it fulfills a certain threshold
+        if str(h)[:THRESHOLD]==comp:
+            hashdict[nonce] = h
+
+    for h in hashdict:
+        print (str(h) + ": " + str(hashdict[h]))
+
+    #DEBUG
+    print("--- %s seconds ---" % (time.time() - start_time))
