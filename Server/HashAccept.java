399ecdf4008b02608c8d23c6af696b37044a6d93
Jason Yik <jyik@usc.edu>
2019-11-30 15:58:47 -0800

    ready

--- Server/HashAccept.java
+++ Server/HashAccept.java
@@ -1,4 +1,9 @@
-packet ProjectServer;
+package ProjectServer;
+
+import java.net.DatagramPacket;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class HashAccept extends Thread {
 	Connection conn;
@@ -20,33 +25,29 @@ public class HashAccept extends Thread {
 	public HashAccept(Connection conn, DatagramPacket recv) {
 		this.conn = conn;
 		this.recvmsg = recv.getData();
-		this.recvip = recv.getSocketAddress().toString(); //might have to cast SocketAddress to InetSocketAddress
+		this.recvip = recv.getSocketAddress().toString(); 
 
 		this.start();
 	}
 
 	public void run() {
 		try {
-			String msg = toString(recv);
+			String msg = toString(recvmsg);
 			String[] data = msg.split(" ");
 
 			String result = data[0];
 			int compute_time = Integer.parseInt(data[1]);
-
-			try {
-				//add info to database
-				PreparedStatement ps = null;
-				ps = conn.prepareStatement("INSERT INTO hashes (compute_time, address, result) VALUES (?,?,?)");
-				ps.setInt(1, compute_time);
-				ps.setString(2, recvip);
-				ps.setString(3, result);
-				ps.executeUpdate();
-			}
 			
+			//add info to database
+			PreparedStatement ps = null;
+			ps = conn.prepareStatement("INSERT INTO hashes (compute_time, address, result) VALUES (?,?,?)");
+			ps.setInt(1, compute_time);
+			ps.setString(2, recvip);
+			ps.setString(3, result);
+			ps.executeUpdate();
 			if(ps != null) {
 				ps.close();
 			}
-
 		}
 		catch (SQLException sqle) {
 			System.out.println(sqle.getMessage());
--- Server/PacketServer.java
+++ Server/PacketServer.java
@@ -1,5 +1,13 @@
 package ProjectServer;
 
+//need to include mysql connector jars, GOOGLE_APPLICATION_CREDENTIALS json
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PacketServer {
 	public PacketServer() {
 		
@@ -8,7 +16,6 @@ public class PacketServer {
 			byte[] recv = new byte[65535];
 			DatagramPacket dp = null;
 
-			//don't forget to include the mysql connector jars
 			Connection conn = DriverManager.getConnection("jdbc:mysql://google/Project?cloudSqlInstance=myvirus:us-west1:myvir-database&socketFactory=com.google.cloud.sql.mysql.SocketFactory&useSSL=false&user=root&password=root");
 
 			while(true) {
@@ -19,6 +26,9 @@ public class PacketServer {
 
 				recv = new byte[65535];
 			}
+		}
+		catch (SQLException sqle) {
+			System.out.println(sqle.getMessage());
 		}
 		catch (IOException ioe) {
 			System.out.println(ioe.getMessage());
